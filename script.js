110ac4e13757a15b74b82c657dd6528608b28a5b
Suraj-Debnath <surajblp303@gmail.com>
2026-05-28 21:57:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  const menuBtn = document.getElementById("menu-btn");
+ const menuBtn = document.getElementById("menu-btn");
         const hyper = document.querySelector(".hyper");
         menuBtn.addEventListener("click", ()=>{
             if(hyper.style.left === "0px"){
@@ -8,20 +8,21 @@
                 hyper.style.left = "0px";
             }
         });
-        
+
 const searchBar = document.getElementById("searchBar");
-    searchBar.addEventListener("keyup", function(){
-        let searchValue = searchBar.value.toLowerCase();
+    searchBar.addEventListener("keyup", function () {
+        let value = searchBar.value.toLowerCase();
         let items = document.querySelectorAll(
             ".box, .box-1-2, .box-1-3, .box-1-4"
         );
         items.forEach(function(item){
             let text = item.innerText.toLowerCase();
-            if(text.includes(searchValue)){
+            if(text.includes(value)){
                 item.style.display = "block";
+                item.parentNode.prepend(item);
             }
             else{
-                item.style.display = "none";
+                   item.style.display = "none";
             }
         });
-});
+    });
